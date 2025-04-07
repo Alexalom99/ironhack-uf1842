4276a2c507e971e11c3a8e30e2d0b265c5ec6fb7
Alexalom99 <alexalom1999@gmail.com>
2025-04-07 16:36:55 +0200

    dom manipulation

--- dom/app.js
+++ dom/app.js
@@ -1,1 +1,14 @@
 // ¿En tu páis de origen es normal dejar propina?
+
+const bill = document.querySelector("#bill-amount");
+const percent = document.querySelector("#percentage-tip");
+const tip = document.querySelector("#tip-amount");
+const total = document.querySelector("#total");
+const btn = document.querySelector("#calculate");
+
+btn.addEventListener("click", function () {
+    let tipcalc = bill.value * percent.value/100;
+    tip.value = tipcalc;
+    let totalcalc = parseFloat(tipcalc) + parseFloat(bill.value);
+    total.value = totalcalc;
+})
